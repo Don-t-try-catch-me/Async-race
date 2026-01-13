965817ce4dd1fe988c95efc65e38f44236ae0644
Timofey Naryshkin <naryshkint4@gmail..com>
2026-01-13 21:25:51 +0400

    feat: update entry point and default page

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  </div>
-`;
+export const root =
+  document.querySelector('#root') ?? document.createElement('main');
+root.id = 'root';
+
+if (!document.querySelector('#root')) {
+  document.body.append(root);
+}
+
+const h1 = document.createElement('h1')
+h1.textContent = 'Async race'
+root.append(h1)
