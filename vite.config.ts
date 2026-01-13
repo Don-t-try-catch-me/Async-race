cc578884bb20433e0384be1e34d2d8fb084887b0
Timofey Naryshkin <naryshkint4@gmail..com>
2026-01-13 21:14:10 +0400

    feat: add baseline src/ architecture folders

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,8 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src/'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@utils': path.resolve(__dirname, 'src/utils'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+      '@services': path.resolve(__dirname, 'src/services'),
     },
   },
 });
