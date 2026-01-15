ab475c88d77cab619a20e82254009b7cd7a80b7f
Timofey Naryshkin <naryshkint4@gmail..com>
2026-01-15 17:39:02 +0400

    refactor: configure ts rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,23 +3,38 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
   {
-    ignores: ['dist'],
+    ignores: ['dist', 'tests', '**.config.{js,mjs,ts}'],
   },
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
     extends: [
       'js/recommended',
-      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
       eslintPluginUnicorn.configs.recommended,
+      eslintConfigPrettier,
     ],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
     linterOptions: {
       noInlineConfig: true,
       reportUnusedDisableDirectives: true,
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        { assertionStyle: 'never' },
+      ],
+      '@typescript-eslint/no-non-null-assertion': 'error',
     },
   },
 ]);
